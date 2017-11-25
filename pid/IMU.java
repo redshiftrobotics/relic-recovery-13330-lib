b73347d5af7f553a0c58aea203ac574e230778f5
Ari Porad <ari@ariporad.com>
2017-11-25 10:41:06 -0800

    ???

---
+++ pid/IMU.java
@@ -0,0 +1,10 @@
+package org.redshiftrobotics.lib.pid;
+
+/**
+ * Created by adam on 9/16/17.
+ */
+public interface IMU {
+    float getAngularRotationX();
+    float getAngularRotationY();
+    float getAngularRotationZ();
+}
